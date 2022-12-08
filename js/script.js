b0a599e9566129c3f220f29548107ab3549a937d
unknown <saidi.firas@outlook.com>
2022-12-08 22:14:27 +0100

    Updates

--- js/script.js
+++ js/script.js
@@ -14,23 +14,7 @@ document.querySelectorAll('.contact .row .faq .box h3').forEach(faqBox => {
    }
 });
 
-var swiper = new Swiper(".home-slider", {
-   loop:true,
-   effect: "coverflow",
-   spaceBetween: 30,
-   grabCursor: true,
-   coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: false,
-   },
-   navigation: {
-     nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
-   },
-});
+
 
 var swiper = new Swiper(".gallery-slider", {
    loop:true,
